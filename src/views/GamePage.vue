d5f7d1a7af7dae7d75249294545e69f47512b22a
atomych <atomychtrue@gmail.com>
2024-05-11 14:50:31 +0300

    first design for field

--- src/views/GamePage.vue
+++ src/views/GamePage.vue
@@ -23,28 +23,31 @@
         <div class="cell corner start">
           <div class="text"></div>
         </div>
-        <div class="cell default">
+        <div class="cell default red">
           <div class="text">искусство</div>
         </div>
-        <div class="cell default">
+        <div class="cell default red">
           <div class="text">хореография</div>
         </div>
         <div class="cell money default">
-          <div class="text">сбор средств</div>
+          <div class="text">сбор <br />средств</div>
         </div>
-        <div class="cell default">
+        <div class="cell default red">
           <div class="text">актерское мастерство</div>
         </div>
         <div class="cell exam default">
           <div class="text">экзамен</div>
         </div>
-        <div class="cell default">
+        <div class="cell default blue">
+          <div class="text">философия</div>
+        </div>
+        <div class="cell default blue">
           <div class="text">культурология</div>
         </div>
         <div class="cell chance default">
           <div class="text">шанс</div>
         </div>
-        <div class="cell default">
+        <div class="cell default blue">
           <div class="text">социология</div>
         </div>
         <div class="cell corner">
@@ -53,92 +56,101 @@
       </div>
       <div class="right">
         <div class="cell corner">
-          <div class="text">поездка к родителям</div>
+          <div class="text">общежитие</div>
         </div>
-        <div class="cell default">
-          <div class="text">искусство</div>
-        </div>
-        <div class="cell default">
-          <div class="text">хореография</div>
-        </div>
-        <div class="cell money default">
-          <div class="text">сбор средств</div>
-        </div>
-        <div class="cell default">
-          <div class="text">актерское мастерство</div>
-        </div>
-        <div class="cell exam default">
-          <div class="text">экзамен</div>
-        </div>
-        <div class="cell default">
-          <div class="text">культурология</div>
+        <div class="cell default red">
+          <div class="text">правоведение</div>
         </div>
         <div class="cell chance default">
           <div class="text">шанс</div>
         </div>
-        <div class="cell default">
-          <div class="text">социология</div>
+        <div class="cell default red">
+          <div class="text">политология</div>
+        </div>
+        <div class="cell default red">
+          <div class="text">история</div>
+        </div>
+        <div class="cell exam default">
+          <div class="text">экзамен</div>
+        </div>
+        <div class="cell default blue">
+          <div class="text">информатика</div>
+        </div>
+        <div class="cell money default">
+          <div class="text">
+            сбор <br />
+            средств
+          </div>
+        </div>
+        <div class="cell default blue">
+          <div class="text">программиро <br />вание</div>
+        </div>
+        <div class="cell default blue">
+          <div class="text">базы данных</div>
         </div>
         <div class="cell corner">
-          <div class="text">общежитие</div>
+          <div class="text">поездка к родителям</div>
         </div>
       </div>
       <div class="top">
-        <div class="cell default">
-          <div class="text">социология</div>
+        <div class="cell default green">
+          <div class="text">биология</div>
         </div>
         <div class="cell chance default">
           <div class="text">шанс</div>
         </div>
-        <div class="cell default">
-          <div class="text">актерское мастерство</div>
+        <div class="cell default green">
+          <div class="text">почвоведение</div>
         </div>
-        <div class="cell default">
-          <div class="text">актерское мастерство</div>
+        <div class="cell default green">
+          <div class="text">экология</div>
         </div>
         <div class="cell exam default">
           <div class="text">экзамен</div>
         </div>
-        <div class="cell default">
-          <div class="text">социология</div>
+        <div class="cell default blue">
+          <div class="text">экономика</div>
         </div>
         <div class="cell money default">
-          <div class="text">сбор средств</div>
+          <div class="text">
+            сбор <br />
+            средств
+          </div>
         </div>
-        <div class="cell default">
-          <div class="text">социология</div>
+        <div class="cell default blue">
+          <div class="text">менеджмент</div>
         </div>
-        <div class="cell default">
-          <div class="text">социология</div>
+        <div class="cell default blue">
+          <div class="text">управление <br />бизнесом</div>
         </div>
       </div>
       <div class="bottom">
-        <div class="cell default">
-          <div class="text">социология</div>
+        <div class="cell default blue">
+          <div class="text">теория <br />вероятности</div>
         </div>
-        <div class="cell chance default">
-          <div class="text">шанс</div>
-        </div>
-        <div class="cell default">
-          <div class="text">актерское мастерство</div>
-        </div>
-        <div class="cell default">
-          <div class="text">актерское мастерство</div>
-        </div>
-        <div class="cell exam default">
-          <div class="text">экзамен</div>
-        </div>
-        <div class="cell default">
-          <div class="text">социология</div>
+        <div class="cell default blue">
+          <div class="text">линейная <br />алгебра</div>
         </div>
         <div class="cell money default">
           <div class="text">сбор средств</div>
         </div>
-        <div class="cell default">
-          <div class="text">социология</div>
+        <div class="cell default blue">
+          <div class="text">высшая <br />математика</div>
         </div>
-        <div class="cell default">
-          <div class="text">социология</div>
+        <div class="cell exam default">
+          <div class="text">экзамен</div>
+        </div>
+        <div class="cell money default">
+          <div class="text">сбор <br />средств</div>
+        </div>
+        <div class="cell default green">
+          <div class="text">физкультура</div>
+        </div>
+        <div class="cell chance default">
+          <div class="text">шанс</div>
+        </div>
+        <div class="cell default green">
+          <div class="text">бжд</div>
         </div>
       </div>
     </div>
@@ -156,6 +168,8 @@
 
   display: flex;
   justify-content: center;
+
+  padding: 20px 0;
 }
 
 .bar {
@@ -192,6 +206,8 @@
   width: 100%;
   max-width: 1015px;
 
+  background: url("../assets/img/background.png");
+
   .left,
   .right {
     display: flex;
@@ -225,6 +241,67 @@
         transform: rotate(90deg);
       }
     }
+
+    .cell.exam,
+    .cell.money {
+      justify-content: end;
+      position: relative;
+    }
+
+    .cell.exam::before {
+      content: "";
+      display: block;
+      position: absolute;
+
+      width: 71px;
+      height: 60px;
+      background: url("../assets/img/exam.png");
+
+      left: 0;
+      top: 10px;
+
+      transform: rotate(90deg);
+    }
+
+    .cell.money::before {
+      content: "";
+      display: block;
+      position: absolute;
+
+      width: 73px;
+      height: 74px;
+      background: url("../assets/img/money.png");
+
+      left: 0;
+      top: 10px;
+
+      transform: rotate(90deg);
+    }
+
+    .cell.red,
+    .cell.blue {
+      position: relative;
+
+      &::before {
+        content: "";
+        display: block;
+        position: absolute;
+
+        width: 30px;
+        height: 83px;
+
+        top: 0;
+        right: 0;
+      }
+    }
+
+    .cell.red::before {
+      background-color: #ffc568;
+    }
+
+    .cell.blue::before {
+      background-color: #9c76c1;
+    }
   }
 
   .right {
@@ -232,6 +309,67 @@
       .text {
         transform: rotate(-90deg);
       }
+    }
+
+    .cell.exam,
+    .cell.money {
+      justify-content: flex-start;
+      position: relative;
+    }
+
+    .cell.exam::before {
+      content: "";
+      display: block;
+      position: absolute;
+
+      width: 71px;
+      height: 60px;
+      background: url("../assets/img/exam.png");
+
+      right: 0;
+      top: 10px;
+
+      transform: rotate(-90deg);
+    }
+
+    .cell.money::before {
+      content: "";
+      display: block;
+      position: absolute;
+
+      width: 73px;
+      height: 74px;
+      background: url("../assets/img/money.png");
+
+      right: 0;
+      top: 10px;
+
+      transform: rotate(-90deg);
+    }
+
+    .cell.red,
+    .cell.blue {
+      position: relative;
+
+      &::before {
+        content: "";
+        display: block;
+        position: absolute;
+
+        width: 30px;
+        height: 83px;
+
+        top: 0;
+        left: 0;
+      }
+    }
+
+    .cell.red::before {
+      background-color: #fc7877;
+    }
+
+    .cell.blue::before {
+      background-color: #b79ec4;
     }
   }
 
@@ -255,16 +393,80 @@
         text-align: center;
       }
     }
+
+    .cell.money,
+    .cell.exam {
+      padding-top: 10px;
+      align-items: flex-start;
+    }
+
+    .cell.exam {
+      background: url("../assets/img/exam.png") no-repeat 70% 70%;
+    }
+
+    .cell.money {
+      background: url("../assets/img/money.png") no-repeat 70% bottom;
+    }
   }
 
   .top {
     top: 0;
     left: 125px;
+
+    .cell.green,
+    .cell.blue {
+      position: relative;
+
+      &::before {
+        content: "";
+        display: block;
+        position: absolute;
+
+        width: 83px;
+        height: 30px;
+
+        bottom: 0;
+        left: 0;
+      }
+    }
+
+    .cell.green::before {
+      background-color: #7cd55f;
+    }
+
+    .cell.blue::before {
+      background-color: #61cfd4;
+    }
   }
 
   .bottom {
     bottom: 0;
     left: 125px;
+
+    .cell.green,
+    .cell.blue {
+      position: relative;
+
+      &::before {
+        content: "";
+        display: block;
+        position: absolute;
+
+        width: 83px;
+        height: 30px;
+
+        top: 0;
+        left: 0;
+      }
+    }
+
+    .cell.green::before {
+      background-color: #549b70;
+    }
+
+    .cell.blue::before {
+      background-color: #76a6c1;
+    }
   }
 }
 
@@ -274,6 +476,14 @@
   &.corner {
     width: 125px;
     height: 125px;
+  }
+
+  &.start {
+    background: url("../assets/img/start.png") no-repeat center center;
+  }
+
+  &.chance {
+    background: url("../assets/img/star.png") no-repeat center center;
   }
 
   &.chance,
